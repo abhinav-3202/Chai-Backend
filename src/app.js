5773da836c43ebab0c84ecd848ef096dfce1589f
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2025-09-04 22:24:01 +0530

    util code disscussion

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser';
+
+// cookie-parser -->> from my server access user browser cookies and set them also 
+const app = express();
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,
+    credentials : true,
+}))
+
+app.use(express.json({limit : "16kb "})) // from se jo data aaya 
+app.use(express.urlencoded({extended : true , limit : "16kb"})) // url se dta ke liye
+app.use(express.static("public")) // storing something at the server in public folder just now created 
+app.use(cookieParser())
+export { app }
