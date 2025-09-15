39adb3908bcb7eb386f1ab3845504b5593ea96e1
Abhinav Pratap Singh <abhinavp3202@gmail.com>
2025-09-15 18:28:49 +0530

    add middleware and login logout controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,23 @@ import { ApiError } from '../utils/Apierror.js'
 import { User } from '../models/user.models.js'
 import { uploadOnCloudinary } from '../utils/cloudnary.js'
 import { ApiResponse } from '../utils/ApiResponse.js'
+import cookieParser from 'cookie-parser'
+
+const generateAccessAndRefreshTokens = async (userId) =>{
+    try {
+        const user = await User.findById(userId);
+        const refreshToken = user.generateRefreshToken()
+        const accessToken = user.generateAccessToken()
+        user.refreshToken = refreshToken;
+        await user.save({validateBeforeSave : false }) // taaki baaki models na kick in ho jaaye mongoose user ke 
+        // because here sirf refresh tokens save karne ko keh rhe baki fields nhi h save karne ke liye 
+
+        return {accessToken , refreshToken}
+
+    } catch (error) {
+        throw new ApiError(500 , "Something went wrong while generating access and refresh tokens")
+    }
+}
 
 const registerUser = asyncHandler( async (req , res ) =>{
     //get user details from frontend 
@@ -86,4 +103,102 @@ const registerUser = asyncHandler( async (req , res ) =>{
     )
 })
 
-export { registerUser }
+const loginUser = asyncHandler( async (req,res)=>{
+    // take userName and password as input 
+    // check if both fields have characters non-empty
+    // check with DB if both fields exist 
+    // if not give user notification of incorrect credentials 
+    // if entry matches with DB then give user an access token which expires in 10 min 
+    //   +++++++++++++++++++++++++ sir's todos ++++++++++++++++++++++++
+    // req.body se data 
+    // username / email based login 
+    // find the user 
+    // password check 
+    // access and refresh token 
+    // send these token in secure cookies
+    // at the end send res that successfully logined
+
+    const {email , userName , password } = req.body
+
+    if(!userName || !email ){
+        throw new ApiError(400, "userName or email is required ")
+    }
+
+    const user = await User.findOne({
+        $or : [{userName} , {email}]
+    })
+
+    if(!user){
+        throw new ApiError(404 , "user does not exist " )
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+
+    if(!isPasswordValid){
+        throw new ApiError(401 , "Invalid user credentials Password is incorrect " )
+    }
+
+    const {accessToken , refreshToken} = await generateAccessAndRefreshTokens(user._id);
+
+    // after here the user at line 127 does not have access to refreshTokens as it is empty and now will be as the 
+    // call for accessToken and refreshToken was made after user has been called .
+    // so here we have two options either to call DB once again and (OR) to update the previous user .
+    // Here we have to decide whether calling DB once again will be an expensive option or not ...
+    // Here we are doing it by calling the backend query ...
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+    // DB wale User me update krna refreshtokens
+    
+    // now generating cookies 
+    // these are options being generated for assisting cookies 
+    const options = { 
+        httpOnly : true,
+        secure : true // through this only modifiable from server
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken" , accessToken , options)
+    .cookie("refreshToken" , refreshToken , options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user : accessToken,refreshToken, loggedInUser // already saved in cookie then why again sending 
+                // because user might be willing to save those maybe developing mobile app then 
+            },
+            "User loggedIn successfully"
+        )
+    )
+
+})
+
+const logoutUser = asyncHandler( async(req, res) =>{
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set : {
+                refreshToken : undefined,
+            }
+        },
+        {
+            new : true
+        }
+    )
+    const options = { 
+        httpOnly : true,
+        secure : true ,
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken" ,options)
+    .json(new ApiResponse(200 , {} , "User logged Out"))
+} )
+
+export { 
+    registerUser,
+    loginUser,
+    logoutUser,
+}
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,38 @@
+import { User } from "../models/user.models";
+import { ApiError } from "../utils/Apierror";
+import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken";
+// this method will check whether user is there or not 
+
+// if after verification of access and refresh token we got correct user then add a object in req.body named as req.user
+
+export const verifyJWT = asyncHandler(async(req,res,next)=>{
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+        // ? because maybe cookie not present as is not there in the case of mobile apps (no cookies are present there)
+        // maybe user had sent a custom header
+    
+    
+        if(!token ){
+            throw new ApiError(401 , " Unauthorized request ")
+        }
+        // if token present then with the help of jwt we have check if token is correct & to verify what info token has 
+        
+        const decodedToken = jwt.verify(token , process.env.ACCESS_TOKEN_SECRET)
+    
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+        //._id from User.models.js file  in this function userSchema.methods.generateAccessToken
+    
+        if(!user){
+            // NEXT_VIDEO : frontend
+            throw new ApiError(401, "Invalid Access Token")
+        }
+    
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new ApiError(401 , error?.message || "Invalid AccessToken")
+    }
+
+
+})
